7a1a9fe61a3fb51dcce54ec7304ae7e141afad81
Dan <student@epicodus.com>
2015-05-01 10:49:18 -0700

    change the rest of the definition class to handle data.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -26,26 +26,24 @@ describe(Definition) do
       end
     end
 
-    # describe(".clear") do
-    #   it("clears the array of all word/definition pairs") do
-    #       some_word = Definition.new('conflagration')
-    #       some_word.add_def('a great big fire')
-    #       some_word.save()
-    #       Definition.clear()
-    #       expect(Definition.all()).to(eq([]))
-    #   end
-    # end
-    #
-    # describe(".word_def") do
-    #   it("returns an array of all definitions of a particular word") do
-    #       some_word = Definition.new('conflagration')
-    #       another_word = Definition.new('tsunami')
-    #       some_word.add_def('a great big fire')
-    #       some_word.add_def('some big flames')
-    #       another_word.add_def('a great big wave')
-    #       some_word.save()
-    #       another_word.save()
-    #       expect(Definition.word_def('conflagration')).to(eq(['a great big fire', 'some big flames']))
-    #   end
-    # end
+    describe(".clear") do
+      it("clears the array of all word/definition pairs") do
+          some_word = Definition.new('conflagration', 'a great big fire')
+          some_word.save()
+          Definition.clear()
+          expect(Definition.all()).to(eq([]))
+      end
+    end
+
+    describe(".word_def") do
+      it("returns an array of all definitions of a particular word") do
+          some_word = Definition.new('conflagration', 'a great big fire')
+          another_word = Definition.new('tsunami', 'a big wave')
+          some_word2 = Definition.new('conflagration','some big flames')
+          some_word.save()
+          another_word.save()
+          some_word2.save()
+          expect(Definition.word_def('conflagration')).to(eq(['a great big fire', 'some big flames']))
+      end
+    end
 end
